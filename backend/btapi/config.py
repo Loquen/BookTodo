01b988856d6a7300e9f221b3f2189788fb29c274
Loquen Jones <lrjones416@gmail.com>
2020-10-08 16:59:00 -0700

    intial commit

---
+++ backend/btapi/config.py
@@ -0,0 +1,13 @@
+"""
+config.py
+- settings for the flask application object
+"""
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig(object):
+  DEBUG = True
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-hidden-key'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'postgresql://boodoo:ilovereading@localhost/books'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
